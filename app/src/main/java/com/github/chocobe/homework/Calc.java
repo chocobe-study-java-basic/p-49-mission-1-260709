b73c9fb4fc6a4778e3c56fc9fa8067cae4162a51
Chocobe <kyw05171@gmail.com>
2026-07-20 00:01:37 +0900

    리팩토링 및 임시 메모 삭제

--- app/src/main/java/com/github/chocobe/homework/Calc.java
+++ app/src/main/java/com/github/chocobe/homework/Calc.java
@@ -1,66 +1,18 @@
 package com.github.chocobe.homework;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Calc {
-    public Calc() {
-        System.out.println("Calc 인스턴스 생성 완료");
-    }
-
-    /*
-    괄호가 있으면 평탄화 해서 문자열로 반환
-    - `run()` 메소드와 순환 호출
-     */
-    private String flatEval(String eval) {
-        int indexOfStart = eval.indexOf("(");
-        if (indexOfStart < 0) {
-            return eval;
-        }
-
-        // FIXME: 독립된 복수의 괄호 있을 때, 괄호 구조 망가짐
-        // int indexOfEnd = eval.lastIndexOf(")");
-
-        int numOfPairs = 1;
-        int indexOfEnd = -1;
-
-        for (int i = indexOfStart + 1; i < eval.length(); i++) {
-            if (eval.charAt(i) == '(') {
-                numOfPairs++;
-                continue;
-            }
-
-            if (eval.charAt(i) == ')') {
-                indexOfEnd = i;
-                numOfPairs--;
-
-                if (numOfPairs == 0) {
-                    break;
-                }
-            }
-        }
-
-        // if (indexOfStart < 0 && indexOfEnd < 0) {
-        //     return eval;
-        // }
-
-        String subEval = eval.substring(indexOfStart + 1, indexOfEnd);
-        String flatted = this.run(subEval) + "";
-
-        StringBuilder sb = new StringBuilder(eval);
-        sb.replace(indexOfStart, indexOfEnd + 1, flatted);
-
-        return sb.toString();
-    }
-
-    /*
-    TODO: 리스트 가공 연습 필요
+/*
+TODO: 리스트 가공 연습 필요
     - 리스트 중간 요소 수정하기
     - 리스트 중간 요소 n개 삭제하기
     - 리스트 중간에 요소 n개 추가하기
     - 리스트에서 특정 요소의 index 찾기
+ */
 
+public class Calc {
+    /*
     - `flatEval()` 메소드와 순환 함수
      */
     public int run(String eval) {
@@ -71,10 +23,6 @@ public class Calc {
         }
 
         List<String> words = new ArrayList<>(List.of(flattenedEval.split(" ")));
-
-        // FIXME: t26() 실패
-        // String flattedEval = this.flatEval(eval);
-        // List<String> words = new ArrayList<>(List.of(flattedEval.split(" ")));
 
         while (words.size() > 2) {
             int indexOfOperator = 1;
@@ -106,5 +54,43 @@ public class Calc {
         }
 
         return Integer.parseInt(words.getFirst());
+    }
+
+    /*
+    괄호가 있으면 평탄화 해서 문자열로 반환
+    - `run()` 메소드와 순환 호출
+     */
+    private String flatEval(String eval) {
+        int indexOfStart = eval.indexOf("(");
+        if (indexOfStart < 0) {
+            return eval;
+        }
+
+        int numOfPairs = 1;
+        int indexOfEnd = -1;
+
+        for (int i = indexOfStart + 1; i < eval.length(); i++) {
+            if (eval.charAt(i) == '(') {
+                numOfPairs++;
+                continue;
+            }
+
+            if (eval.charAt(i) == ')') {
+                indexOfEnd = i;
+                numOfPairs--;
+
+                if (numOfPairs == 0) {
+                    break;
+                }
+            }
+        }
+
+        String subEval = eval.substring(indexOfStart + 1, indexOfEnd);
+        String flatted = this.run(subEval) + "";
+
+        StringBuilder sb = new StringBuilder(eval);
+        sb.replace(indexOfStart, indexOfEnd + 1, flatted);
+
+        return sb.toString();
     }
 }
